86b68a7e00f8c9f53e37abf5fb2f4469d9fbf542
Juan Felipe Hernandez Palacio <palaciojuanfe@gmail.com>
2022-06-14 18:13:17 -0500

    Re do for proyect

---
+++ src/Doctor.java
@@ -0,0 +1,29 @@
+import javax.print.Doc;
+
+public class Doctor {
+    //Atributos
+    static int id = 0; //Autoincrement
+    String name;
+    String speciality;
+
+    Doctor(){
+        System.out.println("Construyendo el Objeto Doctor");
+
+    }
+
+    Doctor(String name, String speciality){
+
+        System.out.println("El nombre de el doctor asignado es: " + name);
+        id++;
+        this.name = name;
+        this.speciality = speciality;
+    }
+    //Comportamientos
+    public void showName(){
+        System.out.println(name);
+    }
+
+    public void showId(){
+        System.out.println("ID Doctor: " + id);
+    }
+}
