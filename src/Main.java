4466c37a111341e0170fab0004db3c2c719e1822
Juan Felipe Hernandez Palacio <palaciojuanfe@gmail.com>
2022-06-16 16:59:29 -0500

    Patient replacement

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,31 @@ public class Main {
         System.out.println(myDoctor.name);
         System.out.println(myDoctor.speciality);
 
+        int i = 0;
+        int b = 2;
+        b = i;
+        // b = 0
+        String name = "Ann";
+
+
         Patient patient = new Patient("Alejandra", "alejandra@mail.com");
-        patient.setWeight(54.6);
+        Patient patient2 = new Patient("Anahi", "Anahi@mail.com");
+
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+        patient2 = patient;
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+
+        patient2.setName("Manuel");
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+
+        /*patient.setWeight(54.6);
         System.out.println(patient.getWeight());
 
         patient.setPhoneNumber("12345678");
-        System.out.println(patient.getPhoneNumber());
+        System.out.println(patient.getPhoneNumber());*/
     }
 }
 
