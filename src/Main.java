473cc84e8867a22fc031679f6af84bf88f665a4d
Juan Felipe Hernandez Palacio <palaciojuanfe@gmail.com>
2022-07-05 19:24:00 -0500

    Annidation on class Doctor

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
+import java.util.Date;
+
 import static ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
         //showMenu();
-
         Doctor myDoctor = new Doctor("Anahi Salgado", "Pediatria");
-        System.out.println(myDoctor.name);
-        System.out.println(myDoctor.speciality);
+        myDoctor.addAvailableAppointment(new Date(), "4 pm");
+        myDoctor.addAvailableAppointment(new Date(), "10 am");
+        myDoctor.addAvailableAppointment(new Date(), "1 pm");
 
-        int i = 0;
+        for (Doctor.AvailableAppointment aA: myDoctor.getAvailableAppointments()) {
+            System.out.println(aA.getDate() + " " + aA.getTime());
+        }
+
+        /*int i = 0;
         int b = 2;
         b = i;
         // b = 0
@@ -26,13 +32,9 @@ public class Main {
 
         patient2.setName("Manuel");
         System.out.println(patient.getName());
-        System.out.println(patient2.getName());
+        System.out.println(patient2.getName());*/
 
-        /*patient.setWeight(54.6);
-        System.out.println(patient.getWeight());
 
-        patient.setPhoneNumber("12345678");
-        System.out.println(patient.getPhoneNumber());*/
     }
 }
 
