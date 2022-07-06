f0ec24d9775655c269f60988b577dc185978e57a
Juan Felipe Hernandez Palacio <palaciojuanfe@gmail.com>
2022-07-05 21:03:44 -0500

    Polymorphism & overriding method toString into Doctor

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,15 @@ import static ui.UIMenu.*;
 public class Main {
     public static void main(String[] args) {
         //showMenu();
-        Doctor myDoctor = new Doctor("Anahi Salgado", "Pediatria");
+        Doctor myDoctor = new Doctor("Anahi Salgado", "anahi@anahi.com");
         myDoctor.addAvailableAppointment(new Date(), "4 pm");
         myDoctor.addAvailableAppointment(new Date(), "10 am");
         myDoctor.addAvailableAppointment(new Date(), "1 pm");
-
+        System.out.println(myDoctor);
+/*
         for (Doctor.AvailableAppointment aA: myDoctor.getAvailableAppointments()) {
             System.out.println(aA.getDate() + " " + aA.getTime());
-        }
+        }*/
 
         System.out.println();
         System.out.println();
